4b2b3820353b28339f112fff0b447735ed8c1ca8
phrazzld <phrazzld@pm.me>
2025-04-29 23:45:58 +0900

    fix: remove require-related code from Storybook config to fix CI build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,4 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
 import type { StorybookConfig } from "@storybook/nextjs";
-
-// Get the directory name in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const storybookConfig: StorybookConfig = {
   stories: [
